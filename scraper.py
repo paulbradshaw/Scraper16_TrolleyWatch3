ea621e4aa36c29483bc736d5ac0c4b7dbccf46c3
Paul Bradshaw <paulonhismobile@gmail.com>
2017-03-03 20:35:43 +0000

    added different unique key

--- scraper.py
+++ scraper.py
@@ -4,9 +4,11 @@ import lxml.html
 # scrape_table function: gets passed an individual page to scrape
 def scrape_table(root):
     rows = root.cssselect("table.Trolley.table tr")  # selects all <tr blocks within <table class="data"
+    ref = 0
     for row in rows:
         # Set up our data record - we'll need it later
         record = {}
+        ref = ref+1
         table_cells = row.cssselect("td")
         if table_cells:             
             record['Date'] = table_cells[0].text
@@ -14,10 +16,11 @@ def scrape_table(root):
             record['Region'] = table_cells[2].text
             record['Trolley total'] = table_cells[3].text
             record['Ward total'] = table_cells[4].text
+            record['ref'] = ref
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
-            scraperwiki.sqlite.save(["Hospital"], record)
+            scraperwiki.sqlite.save(['ref'], record)
         
 # scrape_and_look_for_next_link function: calls the scrape_table
 # function, then hunts for a 'next' link: if one is found, calls itself again
